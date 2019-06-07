2c759c6281d8b43e3c681c70776079123882a1c2
Rob Paskin <rjpaskin+git@gmail.com>
2019-06-07 13:30:08 +0100

    Fix autoloading issue with Rails 4.2 and Sidekiq
    Sidekiq was failing to boot with a "Cannot define multiple 'included'
    blocks for a Concern" error, caused by the `require` call in the Railtie
    (which itself was added so that the server could boot without a missing
    constant error).
    
    By wrapping the `on_load` callback in an initializer, we can delay the
    callback until the autoload paths have been set up within the app.
    
    See https://stackoverflow.com/a/24486561.

--- lib/csv2db/rails.rb
+++ lib/csv2db/rails.rb
@@ -1,11 +1,9 @@
-require_relative '../../app/controllers/concerns/csv2db/controller_helpers'
-
 module Csv2db
   class Engine < Rails::Engine
     isolate_namespace Csv2db
 
     # See https://content.pivotal.io/blog/leave-your-migrations-in-your-rails-engines
-    initializer :append_migrations do |app|
+    initializer 'csv2db.append_migrations' do |app|
       unless app.root.to_s.match(root.to_s)
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
@@ -13,9 +11,9 @@ module Csv2db
       end
     end
 
-    if defined?(::ActionController)
-      ::ActiveSupport.on_load(:action_controller) do
-        include ::Csv2db::ControllerHelpers
+    initializer 'csv2db.add_controller_helpers' do
+      ActiveSupport.on_load(:action_controller) do
+        include Csv2db::ControllerHelpers
       end
     end
   end
