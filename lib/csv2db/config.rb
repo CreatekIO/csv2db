8c7fe934fa5c99277e95f9d11fefa81ac5995f65
Reiss Johnson <reissjohnson@me.com>
2025-05-22 14:36:06 +0100

    REW-2045 - adds local_storage_host config option to csv2db gem

--- lib/csv2db/config.rb
+++ lib/csv2db/config.rb
@@ -4,11 +4,15 @@ module Csv2db
   class Config
     include Singleton
 
-    attr_writer :storage_adapter
+    attr_writer :storage_adapter, :local_storage_host
 
     def storage_adapter
       @storage_adapter ||= :dragonfly
       ActiveSupport::StringInquirer.new(@storage_adapter.to_s)
+    end
+
+    def local_storage_host
+      @local_storage_host ||= ''
     end
   end
 end
