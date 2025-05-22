9fe686fbce1e059b5c404ec81184517e70903808
Reiss Johnson <reissjohnson@me.com>
2025-05-22 16:26:52 +0100

    REW-2045 - implement expiring link method and use on downloads table

--- app/models/concerns/csv2db/active_storage_adapter.rb
+++ app/models/concerns/csv2db/active_storage_adapter.rb
@@ -1,6 +1,7 @@
 module Csv2db::ActiveStorageAdapter
   extend ActiveSupport::Concern
   FILE_TYPE = 'text/csv'.freeze
+  MAX_EXPIRY = 7.days.to_s.freeze
 
   included do
     has_one_attached :csv_upload
@@ -10,14 +11,33 @@ module Csv2db::ActiveStorageAdapter
 
   def file=(file)
     # Override Dragonfly setter method
+
+    filename = file.original_filename
+
     csv_upload.attach(
       io: File.open(file),
-      filename: file.original_filename,
+      filename: filename,
       content_type: file.content_type
     )
+
+    self.file_name = filename
+  end
+
+  def expiring_link(expires_in: MAX_EXPIRY)
+    return unless csv_upload.present?
+
+    set_current_host_if_local
+
+    csv_upload.service_url(expires_in: expires_in.to_i, disposition: 'attachment')
   end
 
   private
+
+  def set_current_host_if_local
+    return unless Rails.application.config.active_storage.service == :local
+
+    ActiveStorage::Current.host = ReportGenerator.config.local_storage_host
+  end
 
   def check_file_extension
     # very basic check of file extension
